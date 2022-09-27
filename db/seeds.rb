dfe0cb431382d91f33dcbe4c00ad2954c52b4d93
Nicolas Fernandez <nfernandez@itcrowarg.com>
2022-09-27 07:44:57 -0300

    Data seed disable.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-DatabaseCleaner.clean_with :truncation if Rails.env.development?
+# DatabaseCleaner.clean_with :truncation if Rails.env.development?
 
 ####### PROPERTIES SEEDING - START #######s
-icons = ["AccessibleIcon", "AdfScannerIcon", "AddchartIcon", "ArrowCircleLeftIcon", "CarRepairIcon"]
+# icons = ["AccessibleIcon", "AdfScannerIcon", "AddchartIcon", "ArrowCircleLeftIcon", "CarRepairIcon"]
 # images = [
 #   "https://themes.pixelstrap.com/sheltos/assets/images/property/1.jpg",
 #   "https://themes.pixelstrap.com/sheltos/assets/images/property/2.jpg",
@@ -32,49 +32,49 @@ icons = ["AccessibleIcon", "AdfScannerIcon", "AddchartIcon", "ArrowCircleLeftIco
 #   "https://themes.pixelstrap.com/sheltos/assets/images/property/20.jpg"
 # ]
 
-138.times do
+# 138.times do
 
-  property = Property.create({
-      title: Faker::Lorem.sentence(word_count: 3, supplemental: true, random_words_to_add: 2),
-      description: Faker::Lorem.paragraph(sentence_count: 2, supplemental: true, random_sentences_to_add: 2),
-      p_type: Faker::Number.between(from: 0, to: 2),
-      p_status: Faker::Number.between(from: 0, to: 2),
-      operating_since: Faker::Number.between(from: 1950, to: 2022).to_s,
-      price: Faker::Number.within(range: 20.000..300.000),
-      address: Faker::Address.full_address,
-      coordinates: "#{Faker::Address.latitude},#{Faker::Address.longitude}",
-      sq_mts: Faker::Number.between(from: 500, to: 3000),
-      bathroom_amount: Faker::Number.between(from: 0, to: 4),
-      beedroom_amount: Faker::Number.between(from: 0, to: 4)
-  })
+#   property = Property.create({
+#       title: Faker::Lorem.sentence(word_count: 3, supplemental: true, random_words_to_add: 2),
+#       description: Faker::Lorem.paragraph(sentence_count: 2, supplemental: true, random_sentences_to_add: 2),
+#       p_type: Faker::Number.between(from: 0, to: 2),
+#       p_status: Faker::Number.between(from: 0, to: 2),
+#       operating_since: Faker::Number.between(from: 1950, to: 2022).to_s,
+#       price: Faker::Number.within(range: 20.000..300.000),
+#       address: Faker::Address.full_address,
+#       coordinates: "#{Faker::Address.latitude},#{Faker::Address.longitude}",
+#       sq_mts: Faker::Number.between(from: 500, to: 3000),
+#       bathroom_amount: Faker::Number.between(from: 0, to: 4),
+#       beedroom_amount: Faker::Number.between(from: 0, to: 4)
+#   })
 
-  # rand(1..10).times do
-  #   PropertyMedia.create({
-  #     media_path: images[Faker::Number.between(from: 0, to: 19)],
-  #     property: property
-  #   })
-  # end
+#   # rand(1..10).times do
+#   #   PropertyMedia.create({
+#   #     media_path: images[Faker::Number.between(from: 0, to: 19)],
+#   #     property: property
+#   #   })
+#   # end
 
-  rand(1..10).times do
-    PropertyAttribute.create({
-      name: Faker::House.room,
-      icon_name: icons[Faker::Number.between(from: 0, to: 4)],
-      important: Faker::Boolean.boolean(true_ratio: 0.2),
-      p_attribute_type: :feature,
-      p_attribute_value: Faker::Number.between(from: 0, to: 4),
-      property: property
-    })
-  end
+#   rand(1..10).times do
+#     PropertyAttribute.create({
+#       name: Faker::House.room,
+#       icon_name: icons[Faker::Number.between(from: 0, to: 4)],
+#       important: Faker::Boolean.boolean(true_ratio: 0.2),
+#       p_attribute_type: :feature,
+#       p_attribute_value: Faker::Number.between(from: 0, to: 4),
+#       property: property
+#     })
+#   end
 
-  rand(1..10).times do
-    PropertyAttribute.create({
-      name: Faker::Construction.heavy_equipment,
-      icon_name: icons[Faker::Number.between(from: 0, to: 4)],
-      important: Faker::Boolean.boolean(true_ratio: 0.2),
-      p_attribute_type: :facility,
-      property: property
-    })
-  end
-end 
+#   rand(1..10).times do
+#     PropertyAttribute.create({
+#       name: Faker::Construction.heavy_equipment,
+#       icon_name: icons[Faker::Number.between(from: 0, to: 4)],
+#       important: Faker::Boolean.boolean(true_ratio: 0.2),
+#       p_attribute_type: :facility,
+#       property: property
+#     })
+#   end
+# end 
 
 ####### PROPERTIES SEEDING - END #######
