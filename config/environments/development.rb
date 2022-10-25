26cde92d47e22463e12c0c3c94d5f98b8ea0e2f3
Nicolas Fernandez <nico.06.89crc@gmail.com>
2022-10-25 18:29:25 -0300

    wip

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,6 @@
 require "active_support/core_ext/integer/time"
 Rails.application.routes.default_url_options[:host] = 'localhost:3000'
+Rails.application.config.hosts << /.*\.nicolasfernandez\.online/
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
