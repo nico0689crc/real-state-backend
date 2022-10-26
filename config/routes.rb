21979466530fc69a8920c2b205f748059244cd57
Nicolas Fernandez <nico.06.89crc@gmail.com>
2022-10-26 08:34:02 -0300

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,29 @@
 Rails.application.routes.draw do
-  resources :property_attributes
+  constraints subdomain: 'real-estate-api' do
+    resources :property_attributes
 
-  mount_devise_token_auth_for 'User', at: 'auth', controllers: {
-    registrations:  'overrides/registrations',
-    sessions:  'overrides/sessions',
-  }
-  
-  namespace :api, defaults: {format: :json} do
-    namespace :v1 do
-      namespace :admin do
-        resources :properties do
-          resources :medias, only: [:delete] do
-            delete :destroy, to: "properties#medias_destroy", on: :member
+    mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+      registrations:  'overrides/registrations',
+      sessions:  'overrides/sessions',
+    }
+    
+    namespace :api, defaults: {format: :json} do
+      namespace :v1 do
+        namespace :admin do
+          resources :properties do
+            resources :medias, only: [:delete] do
+              delete :destroy, to: "properties#medias_destroy", on: :member
+            end
+          end
+
+          resources :users do
+            patch :update_password, on: :member
           end
         end
 
-        resources :users do
-          patch :update_password, on: :member
+        namespace :public do
+          resources :properties, only: [:index, :show]
         end
-      end
-
-      namespace :public do
-        resources :properties, only: [:index, :show]
       end
     end
   end
