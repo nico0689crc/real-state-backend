3e80de744b95af28503093bf122f88bcde395a98
Nicolas Fernandez <nico.06.89crc@gmail.com>
2022-10-26 08:19:50 -0300

    wip

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,9 +1,9 @@
 require "active_support/core_ext/integer/time"
 Rails.application.routes.default_url_options[:host] = 'localhost:3000'
-Rails.application.config.hosts << /.*\.nicolasfernandez\.online/
+Rails.application.config.hosts << "real-estate-api.nicolasfernandez.online"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << /.*\.nicolasfernandez\.online/
+  config.hosts << "real-estate-api.nicolasfernandez.online"
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
