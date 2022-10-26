70d10aa8bc3524fc6fd61c26fd9341b376f1e649
Nicolas Fernandez <62648191+nico0689crc@users.noreply.github.com>
2022-10-26 18:27:00 -0300

    Update passwords_controller.rb

--- app/controllers/overrides/passwords_controller.rb
+++ app/controllers/overrides/passwords_controller.rb
@@ -5,7 +5,7 @@ module Overrides
       @resource = resource_class.with_reset_password_token(resource_params[:reset_password_token])
 
       render json: {
-        data: @resource
+        data: resource_params[:reset_password_token]
       }
     end
   end
