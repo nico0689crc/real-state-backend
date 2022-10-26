37aa8f309ca6bbbb6b3fab48a1edc959ab1fa476
Nicolas Fernandez <nico.06.89crc@gmail.com>
2022-10-26 18:15:17 -0300

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth', controllers: {
     registrations:  'overrides/registrations',
     sessions:  'overrides/sessions',
+    passwords:  'overrides/passwords'
   }
   
   namespace :api, defaults: {format: :json} do
