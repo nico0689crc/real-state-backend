02325b60ed14ac0cf50f285c89d7aecdd43c3ef0
Nicolas Fernandez <nico.06.89crc@gmail.com>
2022-10-27 05:18:25 -0300

    Sending redirect url on user creation.

--- app/managers/user_manager.rb
+++ app/managers/user_manager.rb
@@ -3,12 +3,13 @@ class UserManager < BaseManager
     User
   end
 
-  def create
+  def create(opts={})
+    opts[:redirect_url] = @params[:redirect_url] if @params[:redirect_url].present?
     @object = User.new(object_params)
     @object.send_password_reset_user_initial = true
     if @object.check_permitted_roles(@current_user, object_params[:user_role])
       if @object.save
-        @object.send_reset_password_instructions
+        @object.send_reset_password_instructions(opts)
         @object 
       else
         return false
